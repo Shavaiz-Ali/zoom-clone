51b6d0af95ef475f75378633f786690d27da65f4
Shavaiz-Ali <shavaizali159@gmail.com>
2025-02-27 18:04:11 +0500

    fix

--- src/app/api/where-by/create-room/route.ts
+++ src/app/api/where-by/create-room/route.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { connectToDatabase } from "@/lib/db";
 import { PreviousMeeting } from "@/schemas/previous-meetings";
 import { currentUser } from "@clerk/nextjs/server";
